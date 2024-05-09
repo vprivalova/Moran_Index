3a927b41e2fbac5bc2817ce4d5911f9335f7d865
vprivalova <privalovaa.vv@gmail.com>
2024-05-09 14:40:58 +0700

    commit1

--- main_2.py
+++ main_2.py
@@ -1,3 +1,14 @@
 import moran_index
 
-moran_index.SpatialWeightMatrix.standartization()
+list_of_places, dict_of_parameter = moran_index.LoadData('test_data.txt').file_processing()
+weight_matrix = moran_index.SpatialWeightMatrix.standartization('matrix nso.xlsx')
+z_stat = moran_index.ZStat.z_statistics(dict_of_parameter)
+lisa = moran_index.LocalMoranIndex.l_index_calculation(weight_matrix, z_stat)
+g_index = moran_index.GlobalMoranIndex(lisa)
+print(weight_matrix)
+print(z_stat)
+print(lisa)
+print(g_index)
+visualizing = moran_index.MoranScatterplot(weight_matrix, z_stat, lisa)
+visualizing.dataframe()
+visualizing.diagram()
--- moran_index.py
+++ moran_index.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class LoadData:
@@ -34,8 +35,8 @@ class SpatialWeightMatrix:
         print('Введите в excel файл данные о границах соседей')
 
     @staticmethod
-    def standartization():
-        df = pd.read_excel('matrix.xlsx', index_col=0)
+    def standartization(file):
+        df = pd.read_excel(file, index_col=0)
         np_array = df.to_numpy()
         ls_array = np_array.tolist()
         for i in range(len(ls_array)):
@@ -49,5 +50,143 @@ class SpatialWeightMatrix:
                     ls_array[i][h] = x
 
         np_array = np.array(ls_array)
-        print(np_array)
+        return np_array
+
+
+class ZStat:
+    @staticmethod
+    def z_statistics(dct_of_param):
+        z_dct = {}
+        params_lst = []
+        sample_avg = 0
+
+        for key in dct_of_param:
+            params_lst.append(dct_of_param[key])
+            sample_avg += dct_of_param[key]
+
+        sample_avg = sample_avg / len(params_lst)
+
+        intermediary = 0
+
+        for elem in params_lst:
+            intermediary += (elem - sample_avg) ** 2
+
+        standard_deviation = (intermediary / len(params_lst)) ** 0.5
+
+        index = 0
+        for key in dct_of_param:
+            z_dct[key] = (params_lst[index] - sample_avg) / standard_deviation
+            index += 1
+
+        return z_dct
+
+
+class LocalMoranIndex:
+    @staticmethod
+    def l_index_calculation(weight_matrix, z_stat):
+        z_stat_vector = []
+        lisa_dict = {}
+        lisa_vector = []
+
+        for key in z_stat:
+            z_stat_vector.append(z_stat[key])
+            lisa_dict[key] = 0
+
+        intrm_array = weight_matrix.tolist()
+
+        for i in range(len(intrm_array)):
+            for j in range(len(intrm_array[i])):
+                intrm_array[i][j] = intrm_array[i][j] * z_stat_vector[i] * z_stat_vector[j]
+
+        for elem in intrm_array:
+            summary = 0
+            for sub_elem in elem:
+                summary += sub_elem
+            lisa_vector.append(summary)
+
+        index = 0
+        for key in lisa_dict:
+            lisa_dict[key] = lisa_vector[index]
+            index += 1
+
+        return lisa_dict
+
+
+class GlobalMoranIndex:
+    def __init__(self, lisa_dict):
+        self.lisa_dict = lisa_dict
+        self.result = 0
+        self.number = 0
+        for key in lisa_dict:
+            self.result += lisa_dict[key]
+            self.number += 1
+
+    def __str__(self):
+        return (f'Значение Ig = {self.result}'
+                f'Значение E(I) = {-1 / (self.number - 1)}')
+
+    def __repr__(self):
+        return self.result
+
+
+class MoranScatterplot:
+    def __init__(self, weight_matrix, z_dict, lisa_dict):
+        self.weight_matrix = weight_matrix
+        self.z_dict = z_dict
+        self.lisa_dict = lisa_dict
+        self.z_vector = []
+        self.l_vector = []
+        self.places = []
+
+        for key in self.z_dict:
+            self.z_vector.append(z_dict[key])
+            self.places.append(key)
+
+        for key in self.lisa_dict:
+            self.l_vector.append(lisa_dict[key])
+
+        self.wz_vector = (np.array(self.weight_matrix) @ np.array(self.z_vector)).tolist()
+
+    def quarter_definition(self):
+        quarter_vector = []
+
+        for i in range(len(self.places)):
+            if self.z_vector[i] > 0 and self.wz_vector[i] > 0:
+                quarter_vector.append('HH')
+            elif self.z_vector[i] < 0 and self.wz_vector[i] > 0:
+                quarter_vector.append('LH')
+            elif self.z_vector[i] < 0 and self.wz_vector[i] < 0:
+                quarter_vector.append('LL')
+            elif self.z_vector[i] > 0 and self.wz_vector[i] < 0:
+                quarter_vector.append('HL')
+            else:
+                quarter_vector.append('NA')
+        return quarter_vector
+
+    def dataframe(self):
+        df = pd.DataFrame({'Муниципальное образование': self.places, 'z': self.z_vector, 'Wz': self.wz_vector,
+                           'LISA': self.l_vector, 'Квадрант': MoranScatterplot.quarter_definition(self)})
+        print(df)
+        with open('diagram_dataframe.xlsx', 'w', encoding='utf8'):
+            df.to_excel('diagram_dataframe.xlsx')
+
+    def diagram(self):
+        figure, axis = plt.subplots()
+        axis.set_title("Диаграмма рессеяния Морана")
+
+        axis.set_xlim(-10, 10)
+        axis.set_ylim(-10, 10)
+        axis.spines["left"].set_position("center")
+        axis.spines["bottom"].set_position("center")
+        axis.spines['top'].set_visible(False)
+        axis.spines['right'].set_visible(False)
+        axis.set_aspect("equal")
+        plt.xlabel('z')
+        plt.ylabel('Wz')
+
+        x = np.array(self.z_vector)
+        y = np.array(self.wz_vector)
+        plt.scatter(x, y, s=12, c="#8B0000", marker="D")
+        plt.show()
+
 
